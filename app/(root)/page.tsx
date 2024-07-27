ab7cb390ea094ea2317031dc4b5ea01eebdbcbfc
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-07-27 14:13:47 +0700

    Cate headerBox and totalBalanceBox in the homepage

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,5 +1,19 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 const Home = () => {
-  return <div></div>;
+  const loggedIn = { firstName: "VienNguyen" };
+  return (
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox type="greeting" title="Wellcome" user={loggedIn?.firstName || "Guest"} subtext="Access & manage your account and transactions efficiently." />
+
+          <TotalBalanceBox accounts={[]} totalBanks={1} totalCurrentBalance={1250.35} />
+        </header>
+      </div>
+    </section>
+  );
 };
 
 export default Home;
