4029c43c0d673c3fb58f10ebec12a84d36c5a716
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-08-03 14:32:23 +0700

    setup plaid, plaidlink  and dwolla

--- components/form/AuthForm.tsx
+++ components/form/AuthForm.tsx
@@ -12,7 +12,7 @@ import CustomInput from "./CustomInput";
 import { Loader2 } from "lucide-react";
 import { useRouter } from "next/navigation";
 import { signIn, signUp } from "@/lib/actions/user.action";
-
+import PlaidLink from "../PlaidLink";
 const AuthForm = ({ type }: { type: string }) => {
   const [user, setUser] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
@@ -33,8 +33,20 @@ const AuthForm = ({ type }: { type: string }) => {
     try {
       //Sign up with Appwrite and create flaid token
       if (type === "sign-up") {
+        const userData = {
+          firstName: values.firstName!,
+          lastName: values.lastName!,
+          email: values.email,
+          address1: values.address1!,
+          city: values.city!,
+          state: values.state!,
+          dateOfBirth: values.dateOfBirth!,
+          ssn: values.ssn!,
+          postalCode: values.postalCode!,
+          password: values.password,
+        };
         //Take data from frontend
-        const newUser = await signUp(values);
+        const newUser = await signUp(userData);
         if (newUser) setUser(newUser);
       }
       if (type === "sign-in") {
@@ -60,37 +72,38 @@ const AuthForm = ({ type }: { type: string }) => {
           <h1 className=" text-26 font-ibm-plex-serif font-bold text-black-1">Horizon</h1>
         </Link>
         <div className=" flex flex-col gap-2 md:gap-3">
-          <h1 className=" font-semibold text-24 lg:text-36">{user ? " Link Account" : type === "sign-in" ? "Sign In" : "Sign Up"}</h1>
+          <h1 className=" font-semibold text-24 lg:text-36">{user ? " Link access Account" : type === "sign-in" ? "Sign In" : "Sign Up"}</h1>
           <p className="text-16 text-gray-600 font-normal">{user ? "Link your account to get started" : "Please enter your details."}</p>
         </div>
       </header>
       {/* Authentication user */}
       {user ? (
-        <div className=" flex flex-col gap-4">{/* Plaid Link */}</div>
+        <div className=" flex flex-col gap-4">
+          <PlaidLink user={user} variant="primary" />
+        </div>
       ) : (
         <>
           <Form {...form}>
             <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-              {type === "sign-in" && (
+              {type === "sign-in" ? (
                 <>
                   <CustomInput name="email" placeholder="Enter your Email" control={form.control} label="Email" />
                   <CustomInput name="password" placeholder="Enter your Password" control={form.control} label="Password" />
                 </>
-              )}
-              {type === "sign-up" && (
+              ) : (
                 <>
                   <div className="flex gap-4">
                     <CustomInput name="firstName" placeholder="Nguyen" control={form.control} label="First Name" />
                     <CustomInput name="lastName" placeholder="Vien" control={form.control} label="Last Name" />
                   </div>
                   <CustomInput name="address1" placeholder="Enter your specific address" control={form.control} label="Address" />
-                  <CustomInput name="city" placeholder="Example: Ho Chi Minh" control={form.control} label="City" />
+                  <CustomInput name="city" placeholder="Example: Newyork" control={form.control} label="City" />
                   <div className="flex gap-4">
-                    <CustomInput name="state" placeholder="Example: Q1" control={form.control} label="State" />
-                    <CustomInput name="postalCode" placeholder="Example: 700000" control={form.control} label="Postal Code" />
+                    <CustomInput name="state" placeholder="Example:NY, CA" control={form.control} label="State" />
+                    <CustomInput name="postalCode" placeholder="Example: 11101" control={form.control} label="Postal Code" />
                   </div>
                   <div className="flex gap-4">
-                    <CustomInput name="dateOfBirth" placeholder="DD-MM-YYYY" control={form.control} label="Date of Birth" />
+                    <CustomInput name="dateOfBirth" placeholder="YYYY-MM-DD" control={form.control} label="Date of Birth" />
                     <CustomInput name="ssn" placeholder="Example: 1234" control={form.control} label="SSN" />
                   </div>
                   <CustomInput name="email" placeholder="Enter your Email" control={form.control} label="Email" />
--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -3,14 +3,30 @@
 import { ID } from "node-appwrite";
 import { createAdminClient, createSessionClient } from "../appwrite.config";
 import { cookies } from "next/headers";
-import { parseStringify } from "../utils";
+import { encryptId, extractCustomerIdFromUrl, parseStringify } from "../utils";
+import { CountryCode, ProcessorTokenCreateRequest, ProcessorTokenCreateRequestProcessorEnum, Products } from "plaid";
+import { plaidClient } from "../plaid";
+import { revalidatePath } from "next/cache";
+import { addFundingSource, createDwollaCustomer } from "./dwolla.actions";
+const { APPWRITE_DATABASE_ID: DATABASE_ID, APPWRITE_USER_COLLECTION_ID: USER_COLLECTION_ID, APPWRITE_BANK_COLLECTION_ID: BANK_COLLECTION_ID } = process.env;
 
-export async function signUp(userData: SignUpParams) {
-  const { email, password, firstName, lastName } = userData;
+export async function signUp({ password, ...userData }: SignUpParams) {
+  const { email, firstName, lastName } = userData;
+  let newUserAccount;
   try {
-    const { account } = await createAdminClient();
-
-    const newUserAccount = await account.create(ID.unique(), email, password, `${firstName} ${lastName}`);
+    const { account, database } = await createAdminClient();
+    //Chekc exist user? Ý tưởng mà nó muốn thể hiện ở đây đó là cho phép viêc tạo tao khoan ban đầu sau đó sẽ liên kêt với tài khoản ngân hàng
+    newUserAccount = await account.create(ID.unique(), email, password, `${firstName} ${lastName}`);
+    if (!newUserAccount) throw new Error("Error while create a new account");
+    const dwollaCustomerUrl = await createDwollaCustomer({ ...userData, type: "personal" });
+    if (!dwollaCustomerUrl) throw new Error("Error while create dwolla customer URL");
+    const dwollaCustomerId = extractCustomerIdFromUrl(dwollaCustomerUrl);
+    const newUser = await database.createDocument(DATABASE_ID!, USER_COLLECTION_ID!, ID.unique(), {
+      ...userData,
+      userId: newUserAccount.$id,
+      dwollaCustomerUrl,
+      dwollaCustomerId,
+    });
     const session = await account.createEmailPasswordSession(email, password);
 
     cookies().set("appwrite-session", session.secret, {
@@ -20,7 +36,7 @@ export async function signUp(userData: SignUpParams) {
       secure: true,
     });
 
-    return parseStringify(newUserAccount);
+    return parseStringify(newUser);
   } catch (error) {
     console.log("Error while create new user", error);
   }
@@ -53,5 +69,88 @@ export const logoutAccount = async () => {
     await account.deleteSession("current");
   } catch (error) {
     return null;
+  }
+};
+
+export const createLinkToken = async (user: User) => {
+  try {
+    const tokenParams = {
+      user: {
+        client_user_id: user.$id,
+      },
+      client_name: `${user.firstName} ${user.lastName}`,
+      products: ["auth"] as Products[],
+      language: "en",
+      country_codes: ["US"] as CountryCode[],
+    };
+
+    const response = await plaidClient.linkTokenCreate(tokenParams);
+    return parseStringify({ linkToken: response.data.link_token });
+  } catch (error) {
+    console.log(error);
+  }
+};
+export const createBankAccount = async ({ userId, bankId, accountId, accessToken, fundingSourceUrl, sharableId }: createBankAccountProps) => {
+  try {
+    const { database } = await createAdminClient();
+    const bankAccount = await database.createDocument(DATABASE_ID!, BANK_COLLECTION_ID!, ID.unique(), {
+      userId,
+      bankId,
+      accountId,
+      accessToken,
+      fundingSourceUrl,
+      sharableId,
+    });
+    return parseStringify(bankAccount);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const exChangePublicToken = async ({ publicToken, user }: exchangePublicTokenProps) => {
+  try {
+    const response = await plaidClient.itemPublicTokenExchange({
+      public_token: publicToken,
+    });
+    const accessToken = response.data.access_token;
+    const itemId = response.data.item_id;
+    //get account information from Plaid using accesToken
+    const accountsResponse = await plaidClient.accountsGet({
+      access_token: accessToken,
+    });
+
+    const accountData = accountsResponse.data.accounts[0];
+    //Create a processor token for Dwolla using the access token adn account ID
+    const request: ProcessorTokenCreateRequest = {
+      access_token: accessToken,
+      account_id: accountData.account_id,
+      processor: "dwolla" as ProcessorTokenCreateRequestProcessorEnum,
+    };
+    const processorTokenResponse = await plaidClient.processorTokenCreate(request);
+    const processorToken = processorTokenResponse.data.processor_token;
+    //read a funding source URL for the account using the dwolla customer ID processor token, and bank name
+    const fundingSourceUrl = await addFundingSource({
+      dwollaCustomerId: user.dwollaCustomerId,
+      processorToken,
+      bankName: accountData.name,
+    });
+
+    //If the fundingSource URL is not create => throw new Error
+    if (!fundingSourceUrl) throw Error;
+    //Creat a bank acccount using the user ID, account ID, access Token, funding source URL, and shareble ID
+    await createBankAccount({
+      userId: user.$id,
+      bankId: itemId,
+      accountId: accountData.account_id,
+      accessToken,
+      fundingSourceUrl,
+      sharableId: encryptId(accountData.account_id),
+    });
+
+    //revalidate the path to felect the changes
+    revalidatePath("/");
+    return parseStringify({ publicTokenExchange: "complete" });
+  } catch (error) {
+    console.log("An error occured while reating exchanging token: ", error);
   }
 };
