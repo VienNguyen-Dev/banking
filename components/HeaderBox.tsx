1ff7ba7d57ffa70d7cbe7cfdcf95f09c96bfd45b
VienNguyen-Dev <149781283+VienNguyen-Dev@users.noreply.github.com>
2024-08-15 16:17:17 +0700

    Add enviroment vairiable and fix space in the home page

--- components/HeaderBox.tsx
+++ components/HeaderBox.tsx
@@ -5,7 +5,7 @@ const HeaderBox = ({ type = "title", title, user, subtext }: HeaderBoxProps) => 
     <div className="header-box">
       <h1 className="header-box-title">
         {title}
-        {type === "greeting" && <span className=" text-bankGradient"> &nbsp; {user}</span>}
+        {type === "greeting" && <span className=" text-bankGradient">&nbsp;{user}</span>}
       </h1>
       <p className="header-box-subtext">{subtext}</p>
     </div>
